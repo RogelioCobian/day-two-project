6cdcb6d272d76311fc4b488f1f11a8e17af40d04
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-05-30 16:40:08 -0700

    worked on modules

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'], // External styles
+  styles: [`
+  h3 {
+    color:green;
+  }
+  `], // Inline styles
+})
+export class AppComponent {
+  title = 'app';
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: 'app-servers', //element selector
+  // selector:`[app-servers]`, //Attribute selector
+  // selector: '.app-servers', // class selector
+
+  // templateUrl: './servers.component.html', //External template
+
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // `, // Internal Template
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
